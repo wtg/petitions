e065712d51570448ad832ebe58b883aee748410d
Zach Nobles <zachnobles04@gmail.com>
2024-11-16 17:55:04 -0500

    Added a couple fonts and the Union colors to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,37 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "cherry": "#D6001C",
+        "black": "#000000",
+        "dark-gray": "#515151",
+        "light-gray": "#7A7777",
+
+        "secondary-red": "#AB2328",
+        "secondary-blue": "#00205B",
+        "secondary-white": "#dcdcdc",
+        "secondary-gray": "#b4b4b4",
+
+        "tint-red": {
+          100: "#EABCAD",
+          200: "#D58570",
+          300: "#C35442"
+        },
+        "tint-blue": {
+          100: "#A5B0CB",
+          200: "#667BA2",
+          300: "2#B517F"
+        },
+        "tint-green": {
+          100: "#D4E6E8",
+          200: "#B3D3D5",
+          300: "#94C0C6"
+        }
       },
+
+      fontFamily: {
+        'display': ['Nunito', 'sans-serif'],
+        'body': ['Roboto', 'Poppins', 'sans-serif'],
+      }
     },
   },
   plugins: [],
