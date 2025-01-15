a77c54da6411d5f773c35f523fe2c8a77f979a57
williamschen23 <chenwill2005@gmail.com>
2025-01-14 21:31:39 -0500

    switch to fully using jwt

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
+// import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { db } from "@/lib/db";
+// import { db } from "@/lib/db";
 import { getUserFromDb } from "./actions/user.actions";
 
 interface Profile {
@@ -70,7 +70,7 @@ const authConfig: NextAuthConfig = {
             },
         }
     ],
-    adapter: DrizzleAdapter(db),
+    // adapter: DrizzleAdapter(db),
     secret: process.env.AUTH_SECRET,
     session: {
         strategy: "jwt",
