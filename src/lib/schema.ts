5d67ffcc7dbac55afd91996bf2ab59f3704153d6
williamschen23 <chenwill2005@gmail.com>
2025-01-28 16:18:48 -0500

    finished changes

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -9,9 +9,6 @@ export const users = pgTable("user", {
     name: text("name"),
     // delete this password for production, we are not using password credential
     password: text("password"),
-    // email: text("email").unique(),
-    // emailVerified: timestamp("emailVerified", { mode: "date" }),
-    // image: text("image"),
 });
 
 export const accounts = pgTable("account",
@@ -22,13 +19,6 @@ export const accounts = pgTable("account",
         type: text("type").$type<AdapterAccountType>().notNull(),
         provider: text("provider").notNull(),
         providerAccountId: text("providerAccountId").notNull(),
-        // refresh_token: text("refresh_token"),
-        // access_token: text("access_token"),
-        // expires_at: integer("expires_at"),
-        // token_type: text("token_type"),
-        // scope: text("scope"),
-        // id_token: text("id_token"),
-        // session_state: text("session_state"),
     },
     (account) => [
         {
