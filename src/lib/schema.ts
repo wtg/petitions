462d04b4c892f22f5f266f0c547ccfe8efa86031
williamschen23 <chenwill2005@gmail.com>
2025-01-28 16:53:37 -0500

    revert unneeded changes

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, text } from 'drizzle-orm/pg-core';
+import { AdapterAccountType } from 'next-auth/adapters';
 
-export const users = pgTable("users", {
+export const users = pgTable("user", {
     id: text("id")
         .primaryKey()
         .$defaultFn(() => crypto.randomUUID()),
@@ -8,3 +9,21 @@ export const users = pgTable("users", {
     // delete this password for production, we are not using password credential
     password: text("password"),
 });
+
+export const accounts = pgTable("account",
+    {
+        userId: text("userId")
+            .notNull()
+            .references(() => users.id, { onDelete: "cascade" }),
+        type: text("type").$type<AdapterAccountType>().notNull(),
+        provider: text("provider").notNull(),
+        providerAccountId: text("providerAccountId").notNull(),
+    },
+    (account) => [
+        {
+            compoundKey: primaryKey({
+                columns: [account.provider, account.providerAccountId],
+            }),
+        },
+    ]
+);
