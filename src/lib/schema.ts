009c05a78df7ed65748f7795d002a0caebac8e79
carrc4 <109031162+Ca1ebCarr@users.noreply.github.com>
2025-01-28 17:01:37 -0500

    Basic Schema Added

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,8 +1,8 @@
 // import { integer, pgTable, primaryKey, text, timestamp } from 'drizzle-orm/pg-core';
-import { pgTable, primaryKey, text } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, text, serial, timestamp, integer } from 'drizzle-orm/pg-core';
 import { AdapterAccountType } from 'next-auth/adapters';
 
-export const users = pgTable("user", {
+export const users = pgTable("users", {
     id: text("id")
         .primaryKey()
         .$defaultFn(() => crypto.randomUUID()),
@@ -11,20 +11,52 @@ export const users = pgTable("user", {
     password: text("password"),
 });
 
-export const accounts = pgTable("account",
-    {
-        userId: text("userId")
-            .notNull()
-            .references(() => users.id, { onDelete: "cascade" }),
-        type: text("type").$type<AdapterAccountType>().notNull(),
-        provider: text("provider").notNull(),
-        providerAccountId: text("providerAccountId").notNull(),
-    },
-    (account) => [
-        {
-            compoundKey: primaryKey({
-                columns: [account.provider, account.providerAccountId],
-            }),
-        },
-    ]
-);
+export const petitions = pgTable("petitions", {
+    name: text().notNull(),
+    id: serial().primaryKey(),
+    description: text().notNull(),
+    author: text().notNull(), 
+    creationDate: timestamp()
+        .defaultNow()
+        .notNull(),
+});
+
+export const tagNames = pgTable("tagNames", {
+    tagId: serial().primaryKey(),
+    tagName: text().notNull(),
+});
+
+export const signatures = pgTable("signatures"{
+    userId: text()
+        .notNull()
+        .references(() => users.id, { onDelete: "cascade" }),
+    petitionId: integer()
+        .notNull()
+        .references(() => petitions.id, { onDelete: "cascade" }),
+}, (table) => ({
+    pk: primaryKey({ columns: [table.userId, table.petitionId] })
+}));
+
+export const responses = pgTable("responses", {
+    petitionId: integer()
+        .notNull()
+        .references(() => petitions.id, { onDelete: "cascade" }),
+    userId: text()
+        .notNull()
+        .references(() => users.id, { onDelete: "cascade" }),
+    response: text().notNull(),
+    responseTime: timestamp()
+    .defaultNow()
+    .notNull(),
+});
+
+export const tagRefs = pgTable("tagRefs", {
+    tagId: integer()
+        .notNull()
+        .references(() => tagNames.tagId, { onDelete: "cascade" }),
+    petitionId: integer()
+        .notNull()
+        .references(() => petitions.id, { onDelete: "cascade" }),
+}, (table) => ({
+    pk: primaryKey({ columns: [table.tagId, table.petitionId] })
+}));
