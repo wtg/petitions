cab895022ae6aa28db8b6c2a28ac542f5624af2e
williamschen23 <chenwill2005@gmail.com>
2025-02-06 08:45:00 -0500

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,7 @@ export default function Home() {
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen">
-                <h1 className="font-display text-5xl text-cherry">I like petitions</h1>
-                <h2 className="font-body text-5xl text-light-gray">Hi Williams</h2>
+                <h1 className="font-display text-5xl text-cherry">RPI Petitions</h1>
                 <a href='/profile' className="text-white">Profile page (protected)</a>
             </div>
         </>
