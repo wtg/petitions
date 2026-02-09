da5c0e89a1e562b7aa4319b7041d3da49ab3782a
williamschen23 <chenwill2005@gmail.com>
2026-02-09 16:52:49 -0500

    add base url

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,7 @@ import * as schema from "@/db/schema";
 import { genericOAuth } from "better-auth/plugins";
 
 export const auth = betterAuth({
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: {
