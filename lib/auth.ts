159efdadcd6f84b3fa2e1ecc8faa239d0fbbc9b9
williamschen23 <chenwill2005@gmail.com>
2026-02-11 11:09:37 -0500

    add authentication = post

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,6 +29,8 @@ export const auth = betterAuth({
                     tokenUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/token",
                     userInfoUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/userinfo",
                     scopes: ["openid", "email", "profile"],
+                    pkce: true,
+                    authentication: "post"
                 }
             ]
         })
