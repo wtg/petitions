9a487bdffcb34832f12999ca393d3d35e44c7586
williamschen23 <chenwill2005@gmail.com>
2026-02-11 11:26:00 -0500

    revert

--- lib/auth.ts
+++ lib/auth.ts
@@ -25,11 +25,10 @@ export const auth = betterAuth({
                     providerId: "shib",
                     clientId: process.env.CLIENT_ID ?? "",
                     clientSecret: process.env.CLIENT_SECRET,
-                    discoveryUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/configuration",
-                    // authorizationUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/authorize",
-                    // tokenUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/token",
-                    // userInfoUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/userinfo",
-                    // scopes: ["openid", "email", "profile"],
+                    authorizationUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/authorize",
+                    tokenUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/token",
+                    userInfoUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/userinfo",
+                    scopes: ["openid", "email", "profile"],
                 }
             ]
         })
