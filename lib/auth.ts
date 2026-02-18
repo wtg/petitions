8616b829fac146e7b25c65cd62f63985f222f95c
williamschen23 <chenwill2005@gmail.com>
2026-02-17 22:54:15 -0500

    remove pcke

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,58 +29,8 @@ export const auth = betterAuth({
                     tokenUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/token",
                     userInfoUrl: "https://shib.auth.rpi.edu/idp/profile/oidc/userinfo",
                     scopes: ["openid", "email", "profile"],
-                    pkce: true,
+                    authentication: 'basic',
                     responseMode: "query",
-                    getToken: async ({ code, redirectURI }) => {
-                        const params = new URLSearchParams({
-                            code,
-                            redirect_uri: redirectURI,
-                            grant_type: "authorization_code",
-                        });
-
-                        const credentials = Buffer.from(
-                            `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-                        ).toString("base64");
-
-                        const response = await fetch(
-                            "https://shib.auth.rpi.edu/idp/profile/oidc/token",
-                            {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/x-www-form-urlencoded",
-                                    "Authorization": `Basic ${credentials}`,
-                                },
-                                body: params.toString(),
-                            }
-                        );
-                        const data = await response.json();
-                        return {
-                            accessToken: data.access_token,
-                            refreshToken: data.refresh_token,
-                            idToken: data.id_token,
-                            accessTokenExpiresAt: new Date(Date.now() + data.expires_in * 1000),
-                            scopes: data.scope?.split(" ") ?? [],
-                            raw: data,
-                        };
-                    },
-                    getUserInfo: async (tokens) => {
-                        const response = await fetch(
-                            "https://shib.auth.rpi.edu/idp/profile/oidc/userinfo",
-                            {
-                                headers: {
-                                    "Authorization": `Bearer ${tokens.accessToken}`,
-                                },
-                            }
-                        );
-                        const data = await response.json();
-                        return {
-                            id: data.sub,
-                            email: data.email,
-                            name: data.name || data.preferred_username || data.given_name,
-                            image: data.picture,
-                            emailVerified: data.email_verified ?? false,
-                        };
-                    }
                 }
             ]
         })
